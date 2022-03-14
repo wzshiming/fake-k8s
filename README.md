253fa0da8b118a885f94370ab608fa99e4ee1266
Shiming Zhang <wzshiming@foxmail.com>
2022-03-14 18:57:42 +0800

    Refactor

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # fake-k8s
+
+``` console
+Usage ./fake-k8s.sh
+COMMAND:
+  create [NAME]
+  delete [NAME]
+ARGUMENTS:
+  NAME: name of the cluster
+        default: 'default'
+FLAGS:
+  -h, --help: show this help
+  -r, --replicas: number of replicas of the node
+        default: '5'
+  -p, --port: port of the apiserver of the cluster
+        default: '8080'
+```
+
+## Cteate cluster
+``` console
+./fake-k8s.sh create c1 -p 8081
+./fake-k8s.sh create c2 -p 8082
+```
+
+## Delete cluster
+
+``` console
+./fake-k8s.sh delete c1
+./fake-k8s.sh delete c2
+```
---
+++ fake-k8s.sh
@@ -0,0 +1,258 @@
+#!/usr/bin/env bash
+
+function incluster_kubeconfig() {
+  local name="${1}"
+  cat <<EOF
+apiVersion: v1
+kind: Config
+clusters:
+  - cluster:
+      server: http://${name}-kube-apiserver:8080
+    name: test
+contexts:
+  - context:
+      cluster: test
+    name: test
+current-context: test
+preferences: {}
+EOF
+}
+
+function docker_compose_file() {
+  local name="${1}"
+  local port="${2}"
+  local replicas="${3}"
+  local kubeconfig_path="${4}"
+  cat <<EOF
+version: "3.1"
+services:
+  etcd:
+    container_name: "${name}-etcd"
+    image: docker.io/wzshiming/etcd:v3.4.3
+    restart: always
+    command:
+      - etcd
+      - --data-dir
+      - /etcd-data
+      - --name
+      - node0
+      - --initial-advertise-peer-urls
+      - http://0.0.0.0:2380
+      - --listen-peer-urls
+      - http://0.0.0.0:2380
+      - --advertise-client-urls
+      - http://0.0.0.0:2379
+      - --listen-client-urls
+      - http://0.0.0.0:2379
+      - --initial-cluster
+      - node0=http://0.0.0.0:2380
+
+  kube_apiserver:
+    container_name: "${name}-kube-apiserver"
+    image: docker.io/wzshiming/kube-apiserver:v1.18.8
+    restart: always
+    ports:
+      - ${port}:8080
+    command:
+      - kube-apiserver
+      - --admission-control
+      - ""
+      - --etcd-servers
+      - http://${name}-etcd:2379
+      - --etcd-prefix
+      - /prefix/registry
+      - --insecure-bind-address
+      - 0.0.0.0
+      - --insecure-port
+      - "8080"
+      - --default-watch-cache-size
+      - "10000"
+    links:
+      - etcd
+
+  kube_controller:
+    container_name: "${name}-kube-controller"
+    image: docker.io/wzshiming/kube-controller-manager:v1.18.8
+    restart: always
+    command:
+      - kube-controller-manager
+      - --kubeconfig
+      - /root/.kube/config
+    links:
+      - kube_apiserver
+    configs:
+      - source: kubeconfig
+        target: /root/.kube/config
+
+  kube_scheduler:
+    container_name: "${name}-kube-scheduler"
+    image: docker.io/wzshiming/kube-scheduler:v1.18.8
+    restart: always
+    command:
+      - kube-scheduler
+      - --kubeconfig
+      - /root/.kube/config
+    links:
+      - kube_apiserver
+    configs:
+      - source: kubeconfig
+        target: /root/.kube/config
+
+  fake_kubelet:
+    container_name: "${name}-fake-kubelet"
+    image: ghcr.io/wzshiming/fake-kubelet/fake-kubelet:v0.3.3
+    restart: always
+    command:
+      - --kubeconfig
+      - /root/.kube/config
+    links:
+      - kube_apiserver
+    configs:
+      - source: kubeconfig
+        target: /root/.kube/config
+    environment:
+      NODE_NAME: ""
+      GENERATE_NODE_NAME: fake-
+      GENERATE_REPLICAS: "${replicas}"
+      CIDR: 10.0.0.1/24
+      NODE_TEMPLATE: |-
+        apiVersion: v1
+        kind: Node
+        metadata:
+          annotations:
+            node.alpha.kubernetes.io/ttl: "0"
+          labels:
+            app: fake-kubelet
+            beta.kubernetes.io/arch: amd64
+            beta.kubernetes.io/os: linux
+            kubernetes.io/arch: amd64
+            kubernetes.io/hostname: {{ .metadata.name }}
+            kubernetes.io/os: linux
+            kubernetes.io/role: agent
+            node-role.kubernetes.io/agent: ""
+            type: fake-kubelet
+          name: {{ .metadata.name }}
+
+configs:
+  kubeconfig:
+    file: ${kubeconfig_path}
+
+networks:
+  default:
+    external:
+      name: ${name}
+
+EOF
+}
+
+function create_cluster() {
+  local name="${1}"
+  local port="${2:-8080}"
+  local replicas="${3:-5}"
+  local full_name="fake-k8s-${name}"
+  local tmpdir="${TMPDIR}/fake-k8s/${name}"
+  mkdir -p "${tmpdir}"
+  incluster_kubeconfig "${full_name}" >"${tmpdir}/kubeconfig"
+  docker_compose_file "${full_name}" "${port}" "${replicas}" "${tmpdir}/kubeconfig" >"${tmpdir}/docker-compose.yaml"
+
+  docker network create "${full_name}"
+
+  docker-compose -p "${full_name}" -f "${tmpdir}/docker-compose.yaml" up -d
+
+  kubectl config set "clusters.${full_name}.server" "http://127.0.0.1:${port}"
+  kubectl config set "contexts.${full_name}.cluster" "${full_name}"
+
+  echo "kubectl --context=${full_name} get node"
+  for i in $(seq 1 10); do
+    kubectl --context="${full_name}" get node >/dev/null 2>&1 && break
+    sleep 1
+  done
+  kubectl --context="${full_name}" get node
+  echo "Created cluster ${full_name}."
+}
+
+function delete_cluster() {
+  local name="${1}"
+  local full_name="fake-k8s-${name}"
+
+  docker-compose -p "${full_name}" down
+
+  docker network rm "${full_name}"
+
+  kubectl config delete-context "${full_name}"
+  kubectl config delete-cluster "${full_name}"
+
+  echo "Deleted cluster ${full_name}."
+}
+
+TMPDIR="${TMPDIR:-/tmp/}"
+
+function usage() {
+  echo "Usage $0"
+  echo "COMMAND:"
+  echo "  create [NAME]"
+  echo "  delete [NAME]"
+  echo "ARGUMENTS:"
+  echo "  NAME: name of the cluster"
+  echo "        default: 'default'"
+  echo "FLAGS:"
+  echo "  -h, --help: show this help"
+  echo "  -r, --replicas: number of replicas of the node"
+  echo "        default: '5'"
+  echo "  -p, --port: port of the apiserver of the cluster"
+  echo "        default: '8080'"
+}
+
+function main() {
+  if [[ "$#" -eq 0 ]]; then
+    usage
+    return 1
+  fi
+
+  local replicas
+  local port
+  local args=()
+
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case ${key} in
+    -r | -r=* | --replicas | --replicas=*)
+      [[ "${key#*=}" != "$key" ]] && replicas="${key#*=}" || { replicas="$2" && shift; }
+      ;;
+    -p | -p=* | --port | --port=*)
+      [[ "${key#*=}" != "$key" ]] && port="${key#*=}" || { port="$2" && shift; }
+      ;;
+    -h | --help)
+      usage
+      exit 0
+      ;;
+    *)
+      args+=("${key}")
+      ;;
+    esac
+    shift
+  done
+
+  if [[ "${#args[*]}" -eq 0 ]]; then
+    usage
+    return 1
+  fi
+
+  local command="${args[0]}"
+  local name="${args[1]:-default}"
+
+  case "${command}" in
+  "create")
+    create_cluster "${name}" "${port}" "${replicas}"
+    ;;
+  "delete")
+    delete_cluster "${name}"
+    ;;
+  *)
+    usage
+    return 1
+    ;;
+  esac
+}
+
+main "${@}"
