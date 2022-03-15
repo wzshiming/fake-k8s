277473a5e54402699e13d54536dd454b5d49342f
Shiming Zhang <wzshiming@foxmail.com>
2022-03-15 13:42:06 +0800

    Add NODE_INITIALIZATION_TEMPLATE

--- fake-k8s.sh
+++ fake-k8s.sh
@@ -132,6 +132,33 @@ services:
             node-role.kubernetes.io/agent: ""
             type: fake-kubelet
           name: {{ .metadata.name }}
+      NODE_INITIALIZATION_TEMPLATE: |-
+        addresses:
+        - address: {{ NodeIP }}
+          type: InternalIP
+        allocatable:
+          cpu: 1k
+          memory: 1Ti
+          pods: 1M
+        capacity:
+          cpu: 1k
+          memory: 1Ti
+          pods: 1M
+        daemonEndpoints:
+          kubeletEndpoint:
+            Port: 0
+        nodeInfo:
+          architecture: amd64
+          bootID: ""
+          containerRuntimeVersion: ""
+          kernelVersion: ""
+          kubeProxyVersion: ""
+          kubeletVersion: fake
+          machineID: ""
+          operatingSystem: Linux
+          osImage: ""
+          systemUUID: ""
+        phase: Running
 
 configs:
   kubeconfig:
